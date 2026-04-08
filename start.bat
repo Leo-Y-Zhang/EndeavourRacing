0cabdaf45d1a75f2ec15f491529d070498651660
GreenPandaTech <268190724+GreenPandaTech@users.noreply.github.com>
2026-04-08 23:27:47 +0100

    Add one-click start.bat launcher

--- start.bat
+++ start.bat
@@ -1,4 +1,2 @@
 @echo off
-title Endeavour Mission Control
-echo Opening Endeavour Mission Control...
-start "" "%~dp0index.html"
+start "" "C:\Users\user\Claude Code Projects\EndeavourMissionControl\index.html"
